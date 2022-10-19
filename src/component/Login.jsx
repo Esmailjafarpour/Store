1776c30ab44b63b68f51db29055f1b27dda94748
nader <esmailjafarpour29@gmail.com>
2022-10-19 18:35:39 +0330

    Create a registration page

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,16 +1,101 @@
 import React ,{useState,useEffect} from 'react';
 
-const Login = () => {
+const Login = (props) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [dirty, setDirty] = useState({
+        email:false,
+        password :false,
+    });
+
+    const [errors, setErrors] = useState({
+        email:[],
+        password :[],
+    });
+
+    const [loginMessage, setLoginMessage] = useState("");
+
 
     useEffect(() => {
         document.title = 'Login'
     }, []);
 
+    const validate = () => {
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    console.log(email,password)
-   
+        let errorsData = {};
+
+        //email
+        errorsData.email = [];
+
+        // email can`t blank
+        if (!email) {
+            errorsData.email.push("Email can`t be blank")
+        }
+
+        const validEmailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+        if (email) {
+            if (!validEmailRegex.test(email)) {
+                errorsData.email.push("Proper email address is expected")
+            }
+        }
+
+        //password
+        errorsData.password = [];
+
+        // password can`t blank
+        if (!password) {
+            errorsData.password.push("Password can`t be blank")
+        }
+
+        const validPasswordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,15}$/gm;
+        if (password) {
+            if (!validPasswordRegex.test(password)) {
+                errorsData.password.push("Password should be 6 to 15 charecters long with at least one uppercase letter,one lowercase letter and on digit")
+            }
+        }
+
+        setErrors(errorsData)
+    }
+
+    useEffect(validate, [email,password]);
+
+    const onLoginClick = async() => {
+
+        let dirtyData = dirty;
+        Object.keys(dirty).forEach(control => {
+            dirtyData[control] = true
+        });
+
+        setDirty(dirtyData);
+        validate();
+
+        if (isValid()) {
+            let response = await fetch(`http://localhost:5000/users?email=${email}&password=${password}`,{method:"GET"});
+            if (response.ok) {
+                let responseBody = await response.json();
+                if (responseBody.length>0) {
+                    props.history.replace("/dashboard")
+                }else if(""){
+                    setLoginMessage(<span className="text-danger">Invalid Login,please try again</span>)
+                }else{
+                    setLoginMessage(<span className="text-danger">Unable to connect to server</span>)
+
+                }
+            }
+        }
+    }
+
+    const isValid = () => {
+        let valid = true;
+        for (const control in errors) {
+           if (errors[control].length>0) valid = false;
+        }
+        return valid
+    }
+       
+    
     return(
         <div className="row">
             <div className="col-lg-5 col-md-7 mx-auto">
@@ -31,7 +116,14 @@ const Login = () => {
                                 placeholder="Enter Your Email" name="email"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
+                                onBlur={() => {
+                                    setDirty({...dirty, email:true});
+                                    validate();
+                                }}
                             />
+                            <div className="text-danger">
+                                {dirty["email"]&&errors['email'][0] ? errors['email'] : ""}
+                            </div>
                         </div>
                         <div className="form-group mt-3" >
                             <label htmlFor="password"className="mb-3">password</label>
@@ -43,8 +135,19 @@ const Login = () => {
                                 placeholder="Enter Your password" name="password"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
+                                onBlur={() => {
+                                    setDirty({...dirty, password:true});
+                                    validate();
+                                }}
                             />
+                             <div className="text-danger">
+                                {dirty["password"]&&errors['password'][0] ? errors['password'] : ""}
+                            </div>
                         </div>
+                    </div>
+                    <div className="card-footer text-center">
+                        <div className="m-1">{loginMessage}</div>
+                        <button className="btn btn-success m-2"onClick={onLoginClick}>Login</button>
                     </div>
                 </div>
             </div>
--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -1,6 +1,6 @@
 import React , {useState,useEffect} from 'react';
 
-const Register = () => {
+const Register = (props) => {
 
     const [state, setState] = useState({
         email:"",
@@ -23,9 +23,114 @@ const Register = () => {
         {id:8,countryName:"Canada"} 
     ]);
 
+    const [errors, setErrors] = useState({
+        email:[],
+        password:[],
+        fullName:[],
+        dateOfBrith:[],
+        gender:[],
+        country:[],
+        recieveNewsLetters:[]
+    });
+
+    const [dirty, setDirty] = useState({
+        email:false,
+        password:false,
+        fullName:false,
+        dateOfBrith:false,
+        gender:false,
+        country:false,
+        recieveNewsLetters:false
+    });
+
+    const [message, setMessage] = useState();
+
+    const validate = () => {
+
+        let errorsData = {};
+
+        // email
+        errorsData.email = [];
+
+        // email can`t blank
+        if (!state.email) {
+            errorsData.email.push("Email can`t blank")
+        }
+
+        // email regex
+        const validEmailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+        if (state.email) {
+            if (!validEmailRegex.test(state.email)) {
+                errorsData.email.push("Proper email address is expected")
+            }
+        }
+
+        // password
+        errorsData.password = [];
+
+        // password can`t blank
+        if (!state.password) {
+             errorsData.password.push("Password can`t blank")
+        }
+ 
+        // password regex
+        const validPasswordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,15}$/gm;
+        if (state.password) {
+            if (!validPasswordRegex.test(state.password)) {
+                 errorsData.password.push("Password should be 6 to 15 charecters long with at least one uppercase letter,one lowercase letter and on digit")
+            }
+        }
+
+        // fullName
+        errorsData.fullName = [];
+
+        // fullName can`t blank
+        if (!state.fullName) {
+            errorsData.fullName.push("Full Name can`t blank")
+        }
+
+        // DateOfBrith
+        errorsData.dateOfBrith = [];
+
+        //  can`t DateOfBrith blank
+        if (!state.dateOfBrith) {
+            errorsData.dateOfBrith.push("Date Of Brith can`t blank")
+        }
+
+        // Gender
+        errorsData.gender = [];
+
+        //  can`t Gender blank
+        if (!state.gender) {
+            errorsData.gender.push("Please select Gender either male for female")
+        }
+
+        // Country
+        errorsData.country = [];
+
+        //  can`t Country blank
+        if (!state.country) {
+             errorsData.country.push("Please select a Country")
+        }
+
+        setErrors(errorsData)
+
+    }
+
+    useEffect(validate,[state]);
+     
     useEffect(() => {
         document.title = 'Register';
     }, []);
+
+    const onRegisterClick = () => {
+        let dirtyData = dirty;
+        Object.keys(dirty).forEach(control => {
+            dirtyData[control] = true
+        });
+        setDirty(dirtyData);
+        validate();
+    }
 
     return(
        <div className="row">
@@ -33,64 +138,112 @@ const Register = () => {
             <div className="card border-success shadow my-2">
                 <div className="card-header border-bottom border-success">
                     <h4 style={{fontSize:'40px'}} className="text-success text-center">Register</h4>
+                    <ul className="text-danger text-center">
+                        {Object.keys(errors).map((control)=>{
+                            if(dirty[control]) {
+                                return errors[control].map((err)=>{
+                                    return <li key={err}>{err}</li>
+                                })
+                            }else{
+                                return "";
+                            }
+                        })}
+                    </ul>
                 </div>
                 <div className="card-body border-success">
                     {/* Email */}
-                    <div className="form-group form-row">
-                        <label htmlFor="Email" className="col-lg-4 mb-1">Email</label>
-                        <input  
-                            type="text"
-                            id="Email" 
-                            name="email"
-                            className="form-control mb-2" 
-                            value={state.email}
-                            placeholder="example@gmail.com"
-                            onChange={(e) => setState({...state,[e.target.name]: e.target.value})}
-                        />
+                    <div className="row mb-3">
+                        <label htmlFor="Email" className="col-lg-2">Email</label>
+                        <div className="col-lg-10">
+                             <input  
+                                type="text"
+                                id="Email" 
+                                name="email"
+                                className="form-control" 
+                                value={state.email}
+                                placeholder="example@gmail.com"
+                                onChange={(e) => setState({...state,[e.target.name]: e.target.value})}
+                                onBlur={(e) => {
+                                    setDirty({...dirty , [e.target.name] : true});
+                                    validate();
+                                }}
+                            />
+                            <div className="text-danger">
+                                {dirty["email"]&&errors['email'][0] ? errors['email'] : ""}
+                            </div>
+                        </div>
+                       
                     </div>
                     {/* Password */}
-                    <div className="form-group form-row">
-                        <label htmlFor="Password" className="col-lg-4 mb-1">Password</label>
-                        <input  
-                            type="password" 
-                            id="Password"
-                            name="password"
-                            className="form-control mb-2" 
-                            value={state.password}
-                            placeholder="**********"
-                            onChange={(e) => setState({...state,[e.target.name]: e.target.value})}
-                        />
+                    <div className="row mb-3">
+                        <label htmlFor="Password" className="col-lg-2 ">Password</label>
+                        <div className="col-lg-10">
+                            <input  
+                                type="password" 
+                                id="Password"
+                                name="password"
+                                className="form-control mb-2" 
+                                value={state.password}
+                                placeholder="**********"
+                                onChange={(e) => setState({...state,[e.target.name]: e.target.value})}
+                                onBlur={(e) => {
+                                    setDirty({...dirty , [e.target.name] : true});
+                                    validate();
+                                }}
+                            />
+                            <div className="text-danger">
+                                {dirty["password"]&&errors['password'][0] ? errors['password'] : ""}
+                            </div>
+                        </div>    
                     </div>
                     {/* FullName */}
-                    <div className="form-group form-row">
-                        <label htmlFor="FullName" className="col-lg-4 mb-1">FullName</label>
-                        <input  
-                            type="text" 
-                            id="FullName"
-                            name="fullName"
-                            className="form-control mb-2" 
-                            value={state.fullName}
-                            placeholder="Esmail"
-                            onChange={(e) => setState({...state,[e.target.name]: e.target.value})}
-                        />
+                    <div className="row mb-3">
+                        <label htmlFor="FullName" className="col-lg-2">FullName</label>
+                        <div className="col-lg-10">
+                            <input  
+                                type="text" 
+                                id="FullName"
+                                name="fullName"
+                                className="form-control mb-2" 
+                                value={state.fullName}
+                                placeholder="Esmail"
+                                onChange={(e) => setState({...state,[e.target.name]: e.target.value})}
+                                onBlur={(e) => {
+                                    setDirty({...dirty , [e.target.name] : true});
+                                    validate();
+                                }}
+                            />
+                            <div className="text-danger">
+                                {dirty["fullName"]&&errors['fullName'][0] ? errors['fullName'] : ""}
+                            </div>
+                        </div> 
                     </div>
                     {/* DateOfBrith */}
-                    <div className="form-group form-row">
-                        <label htmlFor="DateOfBrith" className="col-lg-4 mb-1">Date Of Brith</label>
-                        <input  
-                            type="date" 
-                            id="DateOfBrith"
-                            name="dateOfBrith"
-                            className="form-control mb-2" 
-                            value={state.dateOfBrith}
-                            placeholder="00-00-00"
-                            onChange={(e) => setState({...state,[e.target.name]: e.target.value})}
-                        />
+                    <div className="row mb-3">
+                        <label htmlFor="DateOfBrith" className="col-lg-3">Date Of Brith</label>
+                        <div className="col-lg-9">
+                            <input  
+                                type="date" 
+                                id="DateOfBrith"
+                                name="dateOfBrith"
+                                className="form-control mb-2" 
+                                value={state.dateOfBrith}
+                                placeholder="00-00-00"
+                                onChange={(e) => setState({...state,[e.target.name]: e.target.value})}
+                                onBlur={(e) => {
+                                    setDirty({...dirty , [e.target.name] : true});
+                                    validate();
+                                }}
+                            />
+                            <div className="text-danger">
+                                {dirty["dateOfBrith"]&&errors['dateOfBrith'][0] ? errors['dateOfBrith'] : ""}
+                            </div>
+                        </div>    
                     </div>
                     {/* Gender */}
-                    <div className="form-group form-row">
-                        <label htmlFor="" className="col-lg-4 mb-1">Gender</label>
-                        <div className="col-lg-8 d-flex justify-content-around w-100">
+                    <div className="row mb-3">
+                        <label htmlFor="" className="col-lg-2">Gender</label>
+                        <div className="col-lg-8 d-flex justify-content-around ">
                             <div className="form-check">
                                 <input  
                                     type="radio" 
@@ -116,32 +269,43 @@ const Register = () => {
                                 <label htmlFor="female" className="form-check-inline mb-1">Female</label>
                             </div>
                         </div>
+                        <div className="text-danger">
+                            {dirty["gender"]&&errors['gender'][0] ? errors['gender'] : ""}
+                        </div>
                     </div>
 
                     {/* country */}
-                    <div className="form-group form-row">
-                        <label htmlFor="Country" className="col-lg-4 mb-1">Country</label>
-                        <div className="col-lg-8">
+                    <div className="row mb-3">
+                        <label htmlFor="Country" className="col-lg-2">Country</label>
+                        <div className="col-lg-10">
                             <select 
                                 multiple={false}  
                                 id="Country"
                                 name="country"
-                                className="form-control mb-2" 
+                                className="form-control" 
                                 value={state.country}
                                 onChange={(e) => setState({...state,[e.target.name]: e.target.value})}
+                                onBlur={(e) => {
+                                    setDirty({...dirty , [e.target.name] : true});
+                                    validate();
+                                }}
                             >
+                                <option value="">Please Select a country</option>
                                 {countries.map((country)=>(
                                     <option key={country.id} value={country.id}>{country.countryName}</option>
                                 ))}
 
                             </select>
                         </div>
+                        <div className="text-danger">
+                            {dirty["country"]&&errors['country'][0] ? errors['country'] : ""}
+                        </div>
                     </div>
                     {/* End Of Country */}
 
                     {/* Recive News Letters */}
-                    <div className="form-group form-row">
-                        <label htmlFor="" className="col-lg-4 mb-1">Recieve News Letters</label>
+                    <div className="row mb-3">
+                        <label htmlFor="" className="col-lg-4">Recieve News Letters</label>
                         <div className="col-lg-8">
                             <div className="form-check">
                                 <input  
@@ -156,8 +320,22 @@ const Register = () => {
                                 <label htmlFor="recieveNewsLetters" className="form-check-inline mb-1">RecieveNewsLetters</label>
                             </div>
                         </div>
+                        
                     </div>
                 </div>
+
+                {/* footer */}
+                <div className="card-footer text-center">
+                    <div className="m-1">{message}</div>
+                    <button 
+                        className="btn btn-primary m-2"
+                        onClick={onRegisterClick}
+                    >
+                        Register
+                    </button>
+                </div>
+                {/* end of footer */}
+
             </div>
          </div>
        </div>
