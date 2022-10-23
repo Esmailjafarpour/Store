dbb3060f4d1cae1e72dc1b43d72555553ff1e17e
nader <esmailjafarpour29@gmail.com>
2022-10-23 17:16:39 +0330

    Creating categories and brands on the store page

--- src/component/Store.jsx
+++ src/component/Store.jsx
@@ -36,8 +36,85 @@ const Store = () => {
 
     },[]);
 
+    const updateBrandIsChecked = (id) => {
+        let brandsData = brands.map((brand)=>{
+            if(brand.id === id) brand.isChecked = !brand.isChecked;
+            return brand
+        })
+
+        setBrands(brandsData)
+    }
+
+    const updateCategoriesChecked = (id) => {
+        let categoriesData = categories.map((categoru)=>{
+            if(categoru.id === id) categoru.isChecked = !categoru.isChecked;
+            return categoru
+        })
+
+        setCategories(categoriesData)
+    }
+
     return(
-        <div>Store</div>
+        <div>
+            <div className="row py-3 header">
+                <div className="col-lg-3">
+                    <h4><i className="fa fa-shopping-bag"></i>Store</h4>
+                </div>
+                <div className="row">
+                    <div className="col-lg-3 py-2">
+                         <div className="my-2">
+                            <h5>Brands</h5>
+                            <ul className="list-group list-group-flush">
+                                {brands.map((brand)=>(
+                                    <li className="list-group-item" key={brand.id}>
+                                        <div className="form-check">
+                                            <input 
+                                                type="checkBox" 
+                                                className="form-check-input"
+                                                value="true"
+                                                checked={brand.isChecked}
+                                                id={`brand${brand.id}`}
+                                                onChange={()=>{
+                                                    updateBrandIsChecked(brand.id)
+                                                }}
+                                            />
+                                            <label htmlFor={`brand${brand.id}`} className="form-check-label">
+                                                {brand.brandName}
+                                            </label>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                         </div>
+
+                         <div className="my-2">
+                            <h5>Categories</h5>
+                            <ul className="list-group list-group-flush">
+                                {categories.map((category)=>(
+                                    <li className="list-group-item" key={category.id}>
+                                        <div className="form-check">
+                                            <input 
+                                                type="checkBox" 
+                                                className="form-check-input"
+                                                value="true"
+                                                checked={category.isChecked}
+                                                id={`category${category.id}`}
+                                                onChange={()=>{
+                                                    updateCategoriesChecked(category.id)
+                                                }}
+                                            />
+                                            <label htmlFor={`category${category.id}`} className="form-check-label">
+                                                {category.categoryName}
+                                            </label>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
